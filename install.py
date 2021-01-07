477325ce96a63102bec1bd5befb7030df439fc84
Aziz <41291367+Aziz961@users.noreply.github.com>
2021-01-07 12:05:30 -0500

    Update install.py

--- install.py
+++ install.py
@@ -17,21 +17,21 @@ NEGATIVE   = "\033[7m"
 CROSSED    = "\033[9m"
 PURPLE     = "\033[0;35m"
 
-files = ['bann/1.txt', 'bann/2.txt', 'bann/3.txt', 'bann/4.txt', 'bann/5.txt', 'bann/6.txt', 'bann/7.txt']
-frames = []
+#files = ['bann/1.txt', 'bann/2.txt', 'bann/3.txt', 'bann/4.txt', 'bann/5.txt', 'bann/6.txt', 'bann/7.txt']
+#frames = []
 
-os.system('cls||clear')
-for name in files:
-    with open(name, 'r', encoding='utf8') as f:
-        frames.append(f.readlines())
-for i in range(2):
-    for frame in frames:
-        print(''.join(frame))
-        time.sleep(0.2)
-        os.system('cls||clear')
+#os.system('cls||clear')
+#for name in files:
+#    with open(name, 'r', encoding='utf8') as f:
+#        frames.append(f.readlines())
+#for i in range(2):
+#    for frame in frames:
+#        print(''.join(frame))
+#        time.sleep(0.2)
+#        os.system('cls||clear')
 
 
-print(f"""{ITALIC}{brown}
+print(f"""{ITALIC}
        ▄█   ▄█▄    ▄████████    ▄████████    ▄████████      
       ███ ▄███▀   ███    ███   ███    ███   ███    ███         
       ███▐██▀     ███    ███   ███    ███   ███    ███         
@@ -61,6 +61,9 @@ print(f"{bold}{green}[2]-SMS Bomber{end}")
 print(f"{bold}{green}[3]-WhatsApp Bomber{end}")
 print(f"{bold}{green}[4]-Instagram Bruter{end}")
 
+print(f"{ITALIC}'* r' - reinstall{end}")
+print(f"{ITALIC}'ref' - refresh Kara Bohca{end}")
+
 
 
 if not os.path.isdir("bohca"):
@@ -69,22 +72,54 @@ os.chdir("bohca")
 
 #a = int(input())
 a = input(f'''{bold}Enter Number:{end}''')
-if '1' in a:
+if a=="ref":
+	os.system("cd")
+	os.system("rm -rf kara_bohca/")
+	os.system("git clone https://github.com/Aziz961/kara_bohca.git")
+	os.chdir("kara_bohca")
+	os.system("python3 install.py")
+if a=="1":
 	os.system("git clone https://github.com/donlachin/yilan.git")
 	os.chdir("yilan")
 	os.system("./yilan.sh")
-if '2' in a:
+elif a=="1 r":
+	os.system("rm -rf yilan/")
+	os.system("git clone https://github.com/donlachin/yilan.git")
+	os.chdir("yilan")
+	os.system("./yilan.sh")
+elif a=="2":
 	os.system("git clone https://github.com/Aziz961/booms.git")
 	os.chdir("booms")
 	os.system("python3 bomb.py")
-if '3' in a:
+elif a=="2 r":
+	os.system("rm -rf booms/")
+	os.system("git clone https://github.com/Aziz961/booms.git")
+	os.chdir("booms")
+	os.system("python3 bomb.py")
+elif a=="3":
 	os.system("git clone https://github.com/Aziz961/WBomb.git")
 	os.chdir("WBomb")
 	os.system("python3 wbomb.py")
-if '4' in a:
+elif a=="3 r":
+	os.system("rm -rf WBomb")
+	os.system("git clone https://github.com/Aziz961/WBomb.git")
+	os.chdir("WBomb")
+	os.system("python3 wbomb.py")
+if a=="4":
 	os.system("git clone https://github.com/donlachin/insta.git")
 	os.chdir("insta")
 	u=input("Enter uermane:")
 	l=input("Enter List:")
 	m=input("Enter mode:")
 	os.system(f'python3 instagram.py {u} {l} -m {m}')
+elif a=="4 r":
+	os.system("rm -rf insta")
+	os.system("git clone https://github.com/donlachin/insta.git")
+	os.chdir("insta")
+	u = input("Enter uermane:")
+	l = input("Enter List:")
+	m = input("Enter mode:")
+	os.system(f'python3 instagram.py {u} {l} -m {m}')
+else:
+	print(f"{bold}{red}Invalid command{end}")
+
